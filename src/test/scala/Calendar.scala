f73d7810722518d7f949531e24205ff145a4c9ae
CptSasa <salim.boutasfat@stud.tu-darmstadt.de>
2023-10-11 13:17:40 +0200

    fixed Replica IDs, removed usage of System.current time and replaced with uuid, refactoring Calendar

--- src/test/scala/Calendar.scala
+++ src/test/scala/Calendar.scala
@@ -7,6 +7,7 @@ import kofre.dotted.Dotted
 import org.scalacheck.{Arbitrary, Gen}
 import org.scalacheck.Prop.forAll
 
+import java.util.UUID
 import scala.collection.mutable.ListBuffer
 
 sealed trait CalOp
@@ -19,67 +20,76 @@ case class Calendar (calendarList: Dotted[AddWinsSet[Int]],replicaID : String) {
   def sum(): Int = {
     calendarList.elements.sum
   }
-//constructs a calender with a given calender and a trace that contains operations which need to be called opon the Calendar
-  def constructCalendar(current: Calendar, remainingTrace: List[CalOp]) : Calendar = {
 
-    if(remainingTrace.nonEmpty){
-    return remainingTrace.head match
-      case AddOp(num) => constructCalendar(current.addCal(current,num), remainingTrace.tail)
-      case RemoveOp(num)=> constructCalendar(current.removeCalendar(current,num),remainingTrace.tail)
-      case MergeOp(other)=> constructCalendar(current.mergeCalendar(current,constructCalendar(Calendar(Dotted.empty,(""+ System.currentTimeMillis())),other)),remainingTrace.tail)
-  }
-    else{
-    return current
-    }
-  }
+  //constructs a calender with a given calender and a trace that contains operations which need to be called opon the Calendar
 
-//  def generateCalendar(depth: Int) : List[CalOp] = {
-//   }
-  def generateCalOp(depth: Int): CalOp ={
-  return generateChoiceWithFrequency(depth) match
-  case 0 => AddOp(generateInt())
-  case 1 => RemoveOp(generateInt())
-  case 2 => MergeOp(generateListOfOp(depth-1))
-}
-  def generateListOfOp(depth: Int): List[CalOp] ={
-    val list =new ListBuffer[CalOp]
-    for (n <- 0 until Gen.choose(depth,5+depth).sample.get){
-      list.addOne(generateCalOp(depth))
-  }
-    return list.toList
-}
-  def addCal(calendar: Calendar, value: Int): Calendar = {
+  def addCal(value: Int): Calendar = {
     //System.out.println(calendar.sum() + " : "+ value+ " Add")
-    val cal = calendar.copy()
+    val cal = this.copy()
     //var tmp = cal.calendarList
-    if (calendar.sum() + value <= 30) {
-      return Calendar(cal.calendarList.add(using (cal.replicaID).asId)(value),calendar.replicaID)
+    if (cal.sum() + value <= 30) {
+      return Calendar(cal.calendarList.add(using (cal.replicaID).asId)(value), cal.replicaID)
     }
     return cal
 
   }
 
-  def removeCalendar(calendar: Calendar, value: Int): Calendar = {
+  def removeCalendar( value: Int): Calendar = {
     //System.out.println(calendar.sum() + " : "+ value+ " Remove")
-    val cal = calendar.copy()
+    val cal = this.copy()
     val tmp = cal.calendarList.remove(value)
-    return Calendar(tmp,cal.replicaID)
+    return Calendar(tmp, cal.replicaID)
   }
 
-  def mergeCalendar(calendar: Calendar, calendar2: Calendar): Calendar = {
+  def mergeCalendar(calendar2: Calendar): Calendar = {
+    val cal = this.copy()
     //System.out.println(calendar.sum() + " Merge "  + calendar2.sum())
-    return Calendar(calendar.calendarList merge calendar2.calendarList,(""+ System.currentTimeMillis()))
+    return Calendar(cal.calendarList merge calendar2.calendarList, UUID.randomUUID().toString)
   }
 
+}
+class CalendarHandler {
   def generateChoiceWithFrequency(depth: Int): Int = {
     val weightedChoices: Gen[Int] = Gen.frequency(
       (5, 0),
       (5, 1),
-      (depth, 2) //default == 3 -> jede Operation hat die gleiche Wahrscheinlichkeit ausgeführt zu werden
+      (depth, 2) //default ==5 -> jede Operation hat die gleiche Wahrscheinlichkeit ausgeführt zu werden
     )
     weightedChoices.sample.get
   }
-  def generateInt(): Int ={
-    return Gen.choose(1,29).sample.get
+
+  def generateInt(): Int = {
+    return Gen.choose(1, 29).sample.get
+  }
+
+  def constructCalendar(current: Calendar, remainingTrace: List[CalOp]): Calendar = {
+
+    if (remainingTrace.nonEmpty) {
+      return remainingTrace.head match
+        case AddOp(num) => constructCalendar(current.addCal(num), remainingTrace.tail)
+        case RemoveOp(num) => constructCalendar(current.removeCalendar(num), remainingTrace.tail)
+        case MergeOp(other) => constructCalendar(current.mergeCalendar(constructCalendar(Calendar(Dotted.empty, UUID.randomUUID().toString), other)), remainingTrace.tail)
+    }
+    else {
+      return current
+    }
+  }
+
+  //  def generateCalendar(depth: Int) : List[CalOp] = {
+  //   }
+  def generateCalOp(depth: Int): CalOp = {
+    return generateChoiceWithFrequency(depth) match
+      case 0 => AddOp(generateInt())
+      case 1 => RemoveOp(generateInt())
+      case 2 => MergeOp(generateListOfOp(depth - 1))
+  }
+
+  def generateListOfOp(depth: Int): List[CalOp] = {
+    val list = new ListBuffer[CalOp]
+    for (n <- 0 until Gen.choose(depth, 5 + depth).sample.get) {
+      list.addOne(generateCalOp(depth))
+    }
+    return list.toList
   }
 }
+
--- src/test/scala/CalendarTest.scala
+++ src/test/scala/CalendarTest.scala
@@ -6,6 +6,8 @@ import kofre.base.Uid.asId
 import kofre.dotted.Dotted
 import org.scalacheck.Prop.{forAll, forAllNoShrink, forAllShrink, lzy}
 import org.scalacheck.{Arbitrary, Gen, Properties, Shrink}
+
+import java.util.UUID
 import util.chaining.scalaUtilChainingOps
 import scala.collection.immutable.List
 import scala.collection.mutable.ListBuffer
@@ -14,96 +16,53 @@ import scala.util.Properties
 
 class CalendarTest {}
   object CalendarSpecification extends Properties("Calendar") {
-    val calendar = Calendar(Dotted.empty,""+ System.currentTimeMillis())
+    val calendar = new CalendarHandler
 
     implicit def traceShrink: Shrink[List[CalOp]] = Shrink { trace =>
       val possiblestates = listAllPossibleOptions(trace)
       println("hi from shrinker!")
-      possiblestates.toStream.filter(x => calendar.constructCalendar(Calendar(Dotted.empty,""+ System.currentTimeMillis()),x).sum()<30)
-//      var reducedTrace = trace
-//      for (n <- trace) {
-//        if (calendar.constructCalendar(Calendar(Dotted.empty), reducedTrace).sum() > 30) {
-//          if(calendar.constructCalendar(Calendar(Dotted.empty), reducedTrace.filter(_ != n)).sum()> 30){
-//            reducedTrace = reducedTrace.filter(_ != n)
-//          }
-//        }
-//      }
+      possiblestates.toStream.filter(x => calendar.constructCalendar(Calendar(Dotted.empty,UUID.randomUUID().toString), x).sum()> 30)
     }
-//    implicit def shrinking(): Shrink[List[CalOp]] = Shrink { trace =>
-//      listAllPossibleOptions(trace).toStream
-//    }
-////    for (n <- operationToReduce) {
-////      if (calender.generateViaTrace(operationToReduce).sum() > 30) {
-////        System.out.println(calender.generateViaTrace(operationToReduce.filter(_.value != n.value)).sum())
-////        if (calender.generateViaTrace(operationToReduce.filter(_.value != n.value)).sum() > 30) {
-////          operationToReduce = operationToReduce.filter(_.value != 2)
-////        }
-////      }
-////    }
-def testConstructCalender(): List[CalOp] = {
-  List(MergeOp(List(AddOp(10),MergeOp(List(AddOp(12),AddOp(13))))),AddOp(11),MergeOp(List(AddOp(2),AddOp(14))))
-}
-//
-//    implicit def shrinkOperation(): Shrink[CalOp] = Shrink { operation =>
-//      operation match
-//        case AddOp(num) => toStream(operation)
-//        case RemoveOp(num) => operation
-//        case MergeOp(other) => MergeOp(Shrink.shrink(other))
-//  }
 
-    def listAllPossibleOptions (list: List[CalOp]): List[List[CalOp]] =
-    {
-        //System.out.println("using this")
-        val possibleStates = new ListBuffer[List[CalOp]]
-        for (n <- list) {
-          n match
-            case AddOp(num) => possibleStates.addOne(list.diff(List(n)))
-            case RemoveOp(num) => possibleStates.addOne(list.diff(List(n)))
-            case MergeOp(other) => {
-              possibleStates.addOne(list.diff(List(n)))
-              val subStates = listAllPossibleOptions(other)
-              for(x <- subStates) {
-                if (x.nonEmpty) {
-                  possibleStates.addOne(list.diff(List(n)).appended(MergeOp(x)))
-                }
+    def listAllPossibleOptions(list: List[CalOp]): List[List[CalOp]] = {
+      //System.out.println("using this")
+      val possibleStates = new ListBuffer[List[CalOp]]
+      for (n <- list) {
+        n match
+          case AddOp(num) => possibleStates.addOne(list.diff(List(n)))
+          case RemoveOp(num) => possibleStates.addOne(list.diff(List(n)))
+          case MergeOp(other) => {
+            possibleStates.addOne(list.diff(List(n)))
+            val subStates = listAllPossibleOptions(other)
+            for (x <- subStates) {
+              if (x.nonEmpty) {
+                possibleStates.addOne(list.diff(List(n)).appended(MergeOp(x)))
+                //possibleStates.addOne(list.updated(list.indexOf(n),MergeOp(x)))
               }
-              }
-              //System.out.println("Possible State :" + possibleStates)
-
-        }
-      possibleStates.toList
-          //List.concat(possibleStates.toList,listAllPossibleOptions(possibleStates.toList.head))
-  }
-          def generateFixed: Gen[List[CalOp]] = testConstructCalender()
-          def generateClass: Gen[List[CalOp]] = calendar.generateListOfOp(7)
-
-          implicit def opGeneratorFixed: Arbitrary[List[CalOp]] = Arbitrary(generateClass)
-
-          val traceOfOneCalFixed = forAll(generateClass) { (generatedTrace: List[CalOp]) =>
-            val generatedCalender = calendar.constructCalendar(calendar, generatedTrace)
-//            System.out.println(generatedTrace)
-//            System.out.println(generatedCalender.sum())
-            generatedCalender.sum() <= 30
+            }
           }
-          val fixed = forAll(generateFixed){ (generatedTrace: List[CalOp]) =>
-//            System.out.println(generatedTrace)
-//            System.out.println(caendar.constructCalendar(calendar, generatedTrace).sum())
-//            System.out.println(calendar.constructCallendar(calendar, generatedTrace).calendarList)
-            calendar.constructCalendar(Calendar(Dotted.empty,""+ System.currentTimeMillis()), generatedTrace).sum() <= 30
-
-          }
-          traceOfOneCalFixed.check()
-          //fixed.check()
+        //System.out.println("Possible State :" + possibleStates)
 
       }
-object Hello {
-  def main(args: Array[String]) = {
-   val testList = List(MergeOp(List(RemoveOp(27), AddOp(15), RemoveOp(20))), RemoveOp(9), MergeOp(List(RemoveOp(28), RemoveOp(28), RemoveOp(7), RemoveOp(14), RemoveOp(11))), AddOp(16), RemoveOp(19), MergeOp(List(AddOp(8), AddOp(9), AddOp(12), MergeOp(List(AddOp(19), RemoveOp(26))), MergeOp(List(AddOp(7), RemoveOp(16), AddOp(26))), RemoveOp(25))), MergeOp(List(AddOp(23), RemoveOp(17), MergeOp(List(RemoveOp(22), AddOp(17), RemoveOp(3), RemoveOp(21))), RemoveOp(29), AddOp(18))), RemoveOp(11))
-    //val testList = List(AddOp(16), MergeOp(List(AddOp(25), AddOp(3), AddOp(26), MergeOp(List(AddOp(6))), AddOp(15), MergeOp(List(MergeOp(List(AddOp(15), AddOp(7), AddOp(11), AddOp(24))))))))//, MergeOp(List(AddOp(7), AddOp(25), AddOp(19), AddOp(22))), AddOp(28), AddOp(12))), AddOp(28))))
-    val cal = Calendar(Dotted.empty,""+ System.currentTimeMillis())
-    val newCalendar = cal.constructCalendar(cal,testList)
-    System.out.println(newCalendar.sum())
-    //System.out.println(newCalendar.calendarList)
-  }
+      possibleStates.toList
+      //List.concat(possibleStates.toList,listAllPossibleOptions(possibleStates.toList.head))
+    }
+
+    def generateClass: Gen[List[CalOp]] = calendar.generateListOfOp(5)
+
+    implicit def opGenerator: Arbitrary[List[CalOp]] = Arbitrary(generateClass)
+
+    val traceOfOneCalFixed = forAll(opGenerator.arbitrary) { (generatedTrace: List[CalOp]) =>
+      val generatedCalender = calendar.constructCalendar(Calendar(Dotted.empty,UUID.randomUUID().toString), generatedTrace)
+      //            System.out.println(generatedTrace)
+                  System.out.println(generatedCalender.sum())
+      generatedCalender.sum() <= 30
+    }
+
+
+    traceOfOneCalFixed.check()
+    //fixed.check()
+
+
 }
 
